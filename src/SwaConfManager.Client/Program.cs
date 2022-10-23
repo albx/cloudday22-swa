cac1925a59d592b463a8ea76b29d6554e55cdf10
Alberto Mori <albi.mo87@hotmail.it>
2022-10-23 11:05:40 +0200

    swa configuration added

--- src/SwaConfManager.Client/Program.cs
+++ src/SwaConfManager.Client/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient<IAgendaClient, AgendaHttpClient>(client => client.BaseAddress = new Uri("http://localhost:7071"));
+builder.Services.AddHttpClient<IAgendaClient, AgendaHttpClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
